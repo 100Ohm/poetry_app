0eaa679a255a525f7de806be93d892c20b75c62f
ohm <811853466@qq.com>
2019-03-19 00:53:37 +0800

    First commit

---
+++ app/src/main/java/xyz/a100ohm/poems/MainActivity.java
@@ -0,0 +1,13 @@
+package xyz.a100ohm.poems;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
