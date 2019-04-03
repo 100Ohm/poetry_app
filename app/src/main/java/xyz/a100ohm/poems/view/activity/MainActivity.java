5ac572b67d45aee92c02d60089ef12383f591043
ohm <811853466@qq.com>
2019-04-04 00:48:24 +0800

    稍微设置了一下styles界面资源，以及新建了mvp架构下的几个主要文件夹。

---
+++ app/src/main/java/xyz/a100ohm/poems/view/activity/MainActivity.java
@@ -0,0 +1,26 @@
+package xyz.a100ohm.poems.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xyz.a100ohm.poems.R;
+
+/**
+ * <p>项目名称: ${PROJECT_NAME} </p>
+ * <p>文件名称: ${FILE_NAME} </p>
+ * <p>描述: [类型描述] </p>
+ * <p>创建时间: ${DATE} ${TIME}</p>
+ * <p>企业信息: 广东工业大学 数字媒体技术专业</p>
+ * @author <a href="mail to: 100ohmYeah@gmail.com" rel="nofollow">一百欧姆</a>
+ * @version v1.0
+ * @update [1][2019.4.4] [一百欧姆][创建文件]
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
